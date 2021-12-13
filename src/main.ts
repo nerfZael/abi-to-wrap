df00195de5034333b5b95a1e49302f02ccf4b349
Jure Bogunovic <bogunovij@gmail.com>
2021-12-13 02:01:20 +0100

    removed test code

--- src/main.ts
+++ src/main.ts
@@ -2,45 +2,15 @@ import { addFirstLast } from "./addFirstLast";
 import { generateCode } from "./generateCode";
 import { loadAbi } from "./loadAbi";
 import { transformAbi } from "./transformAbi";
-import * as Mustache from "mustache";
 
 const abiPath = "./abi.json";
 const templatePath = "./templates/query/query.graphql.template";
 const destPath = "./gen";
 
-const a = {
-  x: "test",
-  arr: [
-    {
-      a: 1
-    },
-    {
-      a: 2
-    },
-    {
-      a: 3
-    }
-  ]
-};
+const abi = loadAbi(abiPath);
 
-const b = {
-  ...a,
-  funcs: a.arr.map(x => x.a.toString()).reduce((s, x) => s + ", " + x, "")
-}
+const transformedAbi = transformAbi(abi);
 
-const output = Mustache.render(`
-import { BigInt } from '{{x}}'
+addFirstLast(transformedAbi);
 
-{{> functions}}
-`, b, {
- "functions": `{{funcs}}`
-});
-console.log(output);
-
-// const abi = loadAbi(abiPath);
-
-// const transformedAbi = transformAbi(abi);
-
-// addFirstLast(transformedAbi);
-
-// generateCode(templatePath, destPath, transformedAbi);
+generateCode(templatePath, destPath, transformedAbi);
